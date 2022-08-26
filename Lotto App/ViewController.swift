1d0b713f4dbb5d9d5beb2e26824fe73380fbfe8b
Dennis Sand <97163222+dennissand@users.noreply.github.com>
2022-08-26 10:29:43 +0200

    Initial Commit

---
+++ Lotto App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lotto App
+//
+//  Created by Dennis Sand on 26.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
